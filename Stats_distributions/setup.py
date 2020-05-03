6336853e251dfa134859c5c2533f1a6b1edca0cc
pratik-1999 <pratikpandav28@gmail.com>
2020-05-03 08:06:17 +0530

    pip setup completed

---
+++ Stats_distributions/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'Stats_distributions',
+      version = '0.1',
+      description = 'Statistical Distributions',
+      packages = ['Stats_distributions'],
+      zip_safe=False)
